79fc12331236dac6ab59423d85359a5bdf982703
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2013-11-16 20:14:16 +0300

    run.py update

--- crawler/run.py
+++ crawler/run.py
@@ -2,4 +2,4 @@ from datetime import date
 from crawler import crawl
 from services import FireService
 
-crawl(service=FireService, from_date=date(year=2013, month=11, day=1), to_date=date(year=2013, month=11, day=1))
+print('\n'.join(crawl(service=FireService, from_date=date(year=2013, month=11, day=1), to_date=date(year=2013, month=11, day=1))))
